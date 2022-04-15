06b6c1b22ef27424588256c5024b85f31f3efc5c
Philipp Schlesinger <info@philcomm.dev>
2022-04-15 18:19:12 +0200

    Add link helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,3 +2,7 @@ def render(component)
   engine = ::Haml::Engine.new(File.open("src/#{component}.haml").read)
   engine.render
 end
+
+def link(path, text)
+  ::Haml::Engine.new("%a{href:\"#{path}\"} #{text}").render
+end
