1d455152b804b1299c653e70be1e7111a8b37f51
Philipp Schlesinger <info@philcomm.dev>
2022-04-15 09:58:01 +0200

    Add webserver

--- lib/server.rb
+++ lib/server.rb
@@ -1,7 +1,20 @@
+require 'thin'
+
 module Statics
   class Server
     def initialize
       puts "This is server"
+
+      app = Rack::Builder.new do
+        map "/" do
+          run Rack::Directory.new("build")
+        end
+      end
+
+      thin_server = Thin::Server.new '127.0.0.1', 3000
+      thin_server.app = app
+
+      thin_server.start
     end
   end
 end
