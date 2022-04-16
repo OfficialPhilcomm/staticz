2184002961c09d104e2c49cfcb690c95ea400024
Philipp Schlesinger <info@philcomm.dev>
2022-04-16 15:48:01 +0200

    Convert scss to compilable

--- lib/manifest/scss.rb
+++ lib/manifest/scss.rb
@@ -2,41 +2,25 @@ require 'sassc'
 
 module Statics
   class Scss
+    include Compilable
+
     attr_reader :name
+
+    def source_file_ending = "scss"
+
+    def build_file_ending = "css"
+
+    def tile_type_name = "Scss"
 
     def initialize(name)
       @name = name
     end
 
-    def path
-      "src/#{name}.scss"
-    end
-
-    def build_path
-      "build/#{name}.css"
-    end
-
     def build
       if exists?
-        engine = ::SassC::Engine.new(File.read(path), syntax: :scss, style: :compressed)
+        engine = ::SassC::Engine.new(File.read(source_path), syntax: :scss, style: :compressed)
 
         File.write build_path, engine.render
-      end
-    end
-
-    def exists?
-      File.exist? path
-    end
-
-    def print(indentation)
-      puts "#{" " * (indentation * 3)}└─ Scss: #{path} #{valid}"
-    end
-
-    def valid
-      if exists?
-        Colors.in_green("✔")
-      else
-        Colors.in_red("✘")
       end
     end
   end
