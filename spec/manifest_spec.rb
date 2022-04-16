491e0f89bf3865a9e02cb59bd11567dd646d390c
Philipp Schlesinger <info@philcomm.dev>
2022-04-16 14:55:11 +0200

    Add specs

---
+++ spec/manifest_spec.rb
@@ -0,0 +1,72 @@
+require_relative "../lib/manifest/manifest"
+
+RSpec.describe "manifest" do
+  context "generate manifest from define" do
+    context "big manifest" do
+      before do
+        Statics::Manifest.define do
+          haml :haml_without_folder
+          sass :sass_without_folder
+          scss :scss_without_folder
+
+          sub :foo do
+            haml :haml_with_folder
+            sass :sass_with_folder
+            scss :scss_with_folder
+          end
+        end
+      end
+
+      it "generates correct manifest" do
+        expect(Statics::Manifest.instance.elements).to match(
+          [
+            have_attributes(name: :haml_without_folder),
+            have_attributes(name: :sass_without_folder),
+            have_attributes(name: :scss_without_folder),
+            have_attributes(name: :foo, elements: match([
+              have_attributes(name: "foo/haml_with_folder"),
+              have_attributes(name: "foo/sass_with_folder"),
+              have_attributes(name: "foo/scss_with_folder")
+            ]))
+          ]
+        )
+      end
+    end
+
+    context "small manifest" do
+      before do
+        Statics::Manifest.define do
+          haml :haml_without_folder
+
+          sub :foo do
+            haml :haml_with_folder
+          end
+        end
+      end
+
+      context "unnested haml" do
+        it "has correct paths generated" do
+          expect(Statics::Manifest.instance.elements.first.path).to eq "src/haml_without_folder.haml"
+          expect(Statics::Manifest.instance.elements.first.build_path).to eq "build/haml_without_folder.html"
+          expect(Statics::Manifest.instance.elements.first.link_path).to eq "/haml_without_folder"
+        end
+      end
+
+      context "nested haml" do
+        let(:haml_object) {
+          Statics::Manifest
+            .instance
+            .elements[1]
+            .elements
+            .first
+        }
+
+        it "has correct paths generated" do
+          expect(haml_object.path).to eq "src/foo/haml_with_folder.haml"
+          expect(haml_object.build_path).to eq "build/foo/haml_with_folder.html"
+          expect(haml_object.link_path).to eq "/foo/haml_with_folder"
+        end
+      end
+    end
+  end
+end
